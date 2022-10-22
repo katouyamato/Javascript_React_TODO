b6ae5f12582817e70b633bc814660773f4a11882
katouyamato <112790178+katouyamato@users.noreply.github.com>
2022-10-22 15:39:16 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -46,15 +46,33 @@
 // console.log(val5);
 
 // テンプレート文字列
-const name = "やまと";
-const age = "24";
+// const name = "やまと";
+// const age = "24";
 
-// 私の名前はやまとです。年齢は24歳です。を出力したい
+// // 私の名前はやまとです。年齢は24歳です。を出力したい
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// アロー関数
+
+// 従来の関数
+// function func1(str){
+//     return str;
+// }
+const func1 = function (str) {
+    return str;
+}
+console.log(func1("func1です。"));
+
+// アロー関数
+const func2 = str => str;
+console.log(func1("func2です。"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
