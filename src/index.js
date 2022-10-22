d37eb020e697089c36d41b8149d0442885bf3bf2
katouyamato <112790178+katouyamato@users.noreply.github.com>
2022-10-22 18:49:53 +0900

    mapやfilter

--- src/index.js
+++ src/index.js
@@ -140,3 +140,32 @@
 // console.log(arr8);
 // arr8[0] = 100;
 // console.log(arr4);
+
+// mapやfilterを使った配列の処理
+// const nameArr = ['田中','山田','大和'];
+// for(let index = 0;index < nameArr.length;index++){
+//     console.log(`${index+1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//     return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name,index)=>console.log(`${index + 1}番目は${name}です。`));
+// nameArr.map((name)=>console.log(`${name}です。`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num)=>{
+//     return num % 2 === 0;
+// })
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name)=>{
+//     if(name === "大和"){
+//         return name;
+//     }else{
+//         return `${name}さん`;
+//     }
+// })
+// console.log(newNameArr);
